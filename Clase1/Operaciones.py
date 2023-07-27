e2589791a2a86351cba0c7f53112b34392f22f41
Daniel Arturo Alfaro Gaitan <36682590+DanielAlfaro1@users.noreply.github.com>
2023-07-27 10:34:34 -0600

    Este es un archivo con operaciones básicas

---
+++ Clase1/Operaciones.py
@@ -0,0 +1,19 @@
+#Operaciones
+a = 6
+print(1+1)      #Operación de suma
+print(a-3)      #Operacion de Resta
+print("Hola el seis es:_"+str(a)) #Concatenación con operador +
+print("Hola el seis es:_", a) #Concatenación con operador ,
+print(a*2)                    #Operacion de multiplicación con números
+print("Hola" * a)             #Operación de multiplicación con un string y número
+
+a = 59                 
+print(a/60)                 #Operación de división
+print(round(a/60))      #Operación de redondeo
+print(a//60)                #Operación de división truncada
+
+a = 3
+b = 2
+print(a**b)                 #Operación de potencia
+print(8%2)                  #Operación modular
+print(3%2)
